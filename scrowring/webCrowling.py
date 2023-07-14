85705660657966d03d488b08f9e384755de25934
dmdrk1414 <seungchan141414@gmail.com>
2023-07-14 11:17:59 +0900

    웹 스크롤링 하는 방법 함수 몇개 쓰기

---
+++ scrowring/webCrowling.py
@@ -0,0 +1,24 @@
+# step1.프로젝트에 필요한 패키지 불러오기
+
+from bs4 import BeautifulSoup as bs
+
+import requests
+
+# step2. 검색할 키워드 입력
+# query = input('검색할 키워드를 입력하세요: ')
+
+# step3. 입력받은 query가 포함된 url 주소(네이버 뉴스 검색 결과 페이지) 저장
+url = 'https://dmdrk1414.tistory.com/'+"49"
+print(url)
+# step4. requests 패키지를 이용해 'url'의 html 문서 가져오기
+
+response = requests.get(url)
+html_text = response.text
+
+# step5. beautifulsoup 패키지로 파싱 후, 'soup' 변수에 저장
+
+soup = bs(html_text, 'html.parser')
+soup = soup.find_all('span')
+# soup = soup.select('')
+
+print(soup)
